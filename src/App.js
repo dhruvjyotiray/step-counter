fb01810a41f1259c0dfca5259dbd696936fe4b03
Dhruvjyoti Ray <dhrubojyoti21@gmail.com>
2023-02-15 23:02:08 +0530

    Redux steps counter

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import StepCounter from "./components/step-counter/step-counter.component.jsx";
+import { legacy_createStore as createStore } from "redux";
 
-function App() {
+const stepCountReducer = (currentState = 0, action) => {
+  switch (action.type) {
+    case "ADD_STEP": {
+      return currentState + 1;
+    }
+    case "RESET": {
+      return 0;
+    }
+    default:
+      return currentState;
+  }
+};
+
+export function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="body">
+        <StepCounter />
+      </div>
     </div>
   );
 }
 
-export default App;
+export const store = createStore(stepCountReducer);
